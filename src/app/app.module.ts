eefc637e53c99cea45eb509f2ce829d5de7633ac
ChHannan <45265763+ChHannan@users.noreply.github.com>
2019-11-07 18:01:56 +0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+let appRoutes: Routes;
+appRoutes = [{path: '', component: LandingPageComponent}, {
+  path: 'login', component: LoginComponent
+}];
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
