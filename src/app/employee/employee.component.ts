a55b8d562b1d67605dc2e9a123209a0764fb303f
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2019-12-18 19:25:57 +0500

    ngIf applied to employee stepper

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 export interface Employee {
   value: string;
@@ -8,18 +9,28 @@ export interface Employee {
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
+  animations: [
+    trigger('fadein', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500, style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class EmployeeComponent implements OnInit {
 
+  employeeType = 'admin';
   employees: Employee[] = [
-    {value: 'value-0', viewValue: 'Doctor'},
-    {value: 'value-1', viewValue: 'Nurse'},
-    {value: 'value-2', viewValue: 'Admin'}
+    {value: 'doctor', viewValue: 'Doctor'},
+    {value: 'nurse', viewValue: 'Nurse'},
+    {value: 'admin', viewValue: 'Admin'}
   ];
   startDate = new Date(1990, 0, 1);
   currentDate = new Date();
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
