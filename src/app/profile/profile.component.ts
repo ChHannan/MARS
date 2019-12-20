a4f4d8b21f6cd7308b673a00f2100c9174ee7ecb
Maaz Bin Tahir <diagaaos@gmail.com>
2019-12-20 22:49:59 +0500

    Fixed profile error

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,20 +1,13 @@
-import {Component, NgModule, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
 
-const MaterialComponents = [MatIconModule];
 
-@NgModule({
-  imports: [MaterialComponents],
-  exports: [MaterialComponents]
-})
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-
 export class ProfileComponent implements OnInit {
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
