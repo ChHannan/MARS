d091fd5784e18afc11c091637da297ee856762ce
Maaz Bin Tahir <diagaaos@gmail.com>
2019-12-21 14:21:49 +0500

    Revert "Revert "REFACTORED the landing page""
    This reverts commit fdfdc57763b4e40ba35cc62e234fe8d12f894664.

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import * as interfaces from '../interfaces';
+import {Router} from "@angular/router";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  TOKEN = '';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      Authorization: ''
+    })
+  };
+  API = 'http://localhost:8000/api/';
+  TOKEN_VIEW = `${this.API}token/`;
+  USER_VIEW = `${this.API}user/`;
+  USER_SELF_VIEW = `${this.API}user/?type=self`;
+  constructor(private http: HttpClient, private router: Router) {
+    if ('token' in localStorage) {
+      this.setToken(localStorage.getItem('token'));
+    } else {
+      this.router.navigateByUrl('http://localhost:8000/login').then();
+    }
+  }
+  setToken(token: string) {
+    this.TOKEN = token;
+    localStorage.setItem('token', token);
+    this.httpOptions.headers.set('Authorization', token);
+  }
+  getToken(cnic: string, password: string) {
+    return this.http.post<interfaces.Token>(this.TOKEN_VIEW, {cnic, password}, this.httpOptions);
+  }
+  getUser() {
+    return this.http.get<interfaces.User[]>(this.USER_SELF_VIEW, this.httpOptions);
+  }
 }
